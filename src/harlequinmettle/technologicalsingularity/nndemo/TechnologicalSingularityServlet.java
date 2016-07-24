8eaa7d9b848d78fe8ca620df27c0721582d65414
Andrew Parelius <parelius@uw.edu>
2016-07-24 11:49:18 -0400

    changed servlet text

--- src/harlequinmettle/technologicalsingularity/nndemo/TechnologicalSingularityServlet.java
+++ src/harlequinmettle/technologicalsingularity/nndemo/TechnologicalSingularityServlet.java
@@ -1,12 +1,15 @@
 package harlequinmettle.technologicalsingularity.nndemo;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class TechnologicalSingularityServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, world.  The technologial singularity may be near");
 	}
 }
